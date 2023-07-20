66ebfe281716d47de75e19932b66265286e43339
Valeria Balducci <v_balducci@web.de>
2023-07-20 14:55:05 +0200

    initialize App.js

--- App.js
+++ App.js
@@ -1,41 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+//import start and chat screen
+import Start from "./components/Start";
+import Chat from "./components/Chat";
 
-// import the screens
-import Screen1 from './components/Screen1';
-import Screen2 from './components/Screen2';
-
-// import react Navigation
-import { NavigationContainer } from '@react-navigation/native';
+//import react navigation
+import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-// Create the navigator
+//create the navigator
 const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="Screen1"
+        initialRouteName="Start"
       >
-        <Stack.Screen
-          name="Screen1"
-          component={Screen1}
+        <Stack.Screen 
+          name="Start"
+          component={Start}
         />
         <Stack.Screen
-          name="Screen2"
-          component={Screen2}
+          name="Chat"
+          component={Chat}
         />
       </Stack.Navigator>
     </NavigationContainer>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
